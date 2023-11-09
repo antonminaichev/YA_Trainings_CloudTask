ef8c86e279da7ad6d7ce53de5da00390558164fe
Tony Minaichev <anton.minaichev@gmail.com>
2023-11-09 12:22:06 -0800

    Instance Group with 2VM

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,27 +8,24 @@ terraform {
 }
 
 provider "yandex" {
-  service_account_key_file = "./tf_key.json"
+  service_account_key_file = "./key.json"
   folder_id                = local.folder_id
-  zone                     = "ru-central1-a"
+  zone                     = local.zone
 }
 
-resource "yandex_vpc_network" "foo" {}
-
-resource "yandex_vpc_subnet" "foo" {
-  zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.foo.id
-  v4_cidr_blocks = ["10.5.0.0/24"]
+data "yandex_vpc_network" "foo" {
+  network_id = "enp9dckkdc3d2bctujsl"
 }
 
-resource "yandex_container_registry" "registry1" {
-  name = "train-registry"
+data "yandex_vpc_subnet" "foo" {
+  subnet_id = "e9bhbi90cm2vac4rm89i"
 }
 
 locals {
-  folder_id = "b1gbgq23ergc1tqrk7am"
+  zone      = "ru-central1-a"
+  folder_id = "b1gre325j3po0m6040ie"
   service-accounts = toset([
-    "catgpt-sa",
+    "catgpt-sa-olada",
   ])
   catgpt-sa-roles = toset([
     "container-registry.images.puller",
@@ -42,16 +39,40 @@ resource "yandex_iam_service_account" "service-accounts" {
 resource "yandex_resourcemanager_folder_iam_member" "catgpt-roles" {
   for_each  = local.catgpt-sa-roles
   folder_id = local.folder_id
-  member    = "serviceAccount:${yandex_iam_service_account.service-accounts["catgpt-sa"].id}"
+  member    = "serviceAccount:${yandex_iam_service_account.service-accounts["catgpt-sa-olada"].id}"
   role      = each.key
 }
 
 data "yandex_compute_image" "coi" {
   family = "container-optimized-image"
 }
-resource "yandex_compute_instance" "catgpt-1" {
+
+resource "yandex_iam_service_account" "ig-sa" {
+  name        = "ig-sa"
+  description = "service account to manage IG"
+}
+
+resource "yandex_resourcemanager_folder_iam_binding" "editor" {
+  folder_id = local.folder_id
+  role      = "editor"
+  members = [
+    "serviceAccount:${yandex_iam_service_account.ig-sa.id}",
+  ]
+  depends_on = [
+    yandex_iam_service_account.ig-sa,
+  ]
+}
+
+resource "yandex_compute_instance_group" "catgpt" {
+  depends_on = [yandex_resourcemanager_folder_iam_binding.editor]
+
+  folder_id          = local.folder_id
+  service_account_id = yandex_iam_service_account.ig-sa.id
+
+  instance_template {
     platform_id        = "standard-v2"
-    service_account_id = yandex_iam_service_account.service-accounts["catgpt-sa"].id
+    service_account_id = yandex_iam_service_account.service-accounts["catgpt-sa-olada"].id
+
     resources {
       cores         = 2
       memory        = 1
@@ -61,20 +82,37 @@ resource "yandex_compute_instance" "catgpt-1" {
       preemptible = true
     }
     network_interface {
-      subnet_id = "${yandex_vpc_subnet.foo.id}"
-      nat = true
+      network_id = data.yandex_vpc_network.foo.id
+      subnet_ids = ["${data.yandex_vpc_subnet.foo.id}"]
+      nat        = true
     }
     boot_disk {
       initialize_params {
-        type = "network-hdd"
-        size = "30"
+        type     = "network-hdd"
+        size     = "30"
         image_id = data.yandex_compute_image.coi.id
       }
     }
     metadata = {
       docker-compose = file("${path.module}/docker-compose.yaml")
-      ssh-keys  = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+      ssh-keys  = "ubuntu:${file("~/.ssh/devops_train.pub")}"
     }
+  }
+
+  scale_policy {
+    fixed_scale {
+      size = 2
+    }
+  }
+
+  allocation_policy {
+    zones = [local.zone]
+  }
+
+  deploy_policy {
+    max_unavailable = 1
+    max_creating    = 1
+    max_expansion   = 1
+    max_deleting    = 1
+  }
 }
-
-
